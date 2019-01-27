14dedbad51dec62f374e38d9947c9d442cd92fbf
Eric Durbin <eadurbin@gmail.com>
2019-01-27 17:50:44 -0600

    isolate Aged Brie logic

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -1,60 +1,57 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (Item item: items) {
-        	if(itemIsSulfuras(item)) {
-        		continue;
-        	}
-        	
-        	if(itemIsBackstagePasses(item)) {
-        		if (itemQualityIsLessThanFifty(item)) {
-        			incrementItemQuality(item);
-                    if (item.sellIn < 11) {
-                    	incrementItemQuality(item);
-                    	
-                        if (item.sellIn < 6) {
-                            incrementItemQuality(item);
-                        }
-                    }
-        		}
-                decrementSellIn(item);
-                if (item.sellIn < 0) {
-                	item.quality = item.quality - item.quality;
-                }
-        	} else {
-                if (!itemIsAgedBrie(item)) {
-                    if (itemQualityIsGreaterThanZero(item)) {
-                    	decrementItemQuality(item);
-                    }
-                } else {
-                    if (itemQualityIsLessThanFifty(item)) {
-                        incrementItemQuality(item);
-                    }
-                }
-                
-                decrementSellIn(item);
-                
-            	if (item.sellIn < 0) {
-            		if (!itemIsAgedBrie(item)) {
-            			if (itemQualityIsGreaterThanZero(item)) {
-            				decrementItemQuality(item);
-            			}
-            		} else {
-            			if (itemQualityIsLessThanFifty(item)) {
-            				incrementItemQuality(item);
-            			}
-            		}
-            	}
-        	}
-        }
-    }
+	public void updateQuality() {
+		for (Item item: items) {
+			if(itemIsSulfuras(item)) {
+				continue;
+			}
+
+			if(itemIsBackstagePasses(item)) {
+				if (itemQualityIsLessThanFifty(item)) {
+					incrementItemQuality(item);
+					if (item.sellIn < 11) {
+						incrementItemQuality(item);
+
+						if (item.sellIn < 6) {
+							incrementItemQuality(item);
+						}
+					}
+				}
+				decrementSellIn(item);
+				if (item.sellIn < 0) {
+					item.quality = item.quality - item.quality;
+				}
+			} else if(itemIsAgedBrie(item)) {
+				if (itemQualityIsLessThanFifty(item)) {
+					incrementItemQuality(item);
+				}
+				decrementSellIn(item);
+
+				if (item.sellIn < 0 && itemQualityIsLessThanFifty(item)) {
+					incrementItemQuality(item);
+				}
+			} else {
+				if (itemQualityIsGreaterThanZero(item)) {
+					decrementItemQuality(item);
+				}
+
+				decrementSellIn(item);
+
+				if (item.sellIn < 0) {
+					if (itemQualityIsGreaterThanZero(item)) {
+						decrementItemQuality(item);
+					}
+				}
+			}
+		}
+	}
 
 	private int decrementSellIn(Item item) {
 		return item.sellIn = item.sellIn - 1;
