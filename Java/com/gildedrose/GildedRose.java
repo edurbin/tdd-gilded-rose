4bb2eb890f8d732571e12d907ffe77bc0fbdd623
Eric Durbin <eadurbin@gmail.com>
2019-01-27 17:17:48 -0600

    extract some duplicated code out to methods

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -11,25 +11,25 @@ class GildedRose {
         for (Item item: items) {
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
+                if (itemQualityIsGreaterThanZero(item)) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
+                        decrementItemQuality(item);
                     }
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                if (itemQualityIsLessThanFifty(item)) {
+                    incrementItemQuality(item);
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                            if (itemQualityIsLessThanFifty(item)) {
+                                incrementItemQuality(item);
                             }
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                            if (itemQualityIsLessThanFifty(item)) {
+                                incrementItemQuality(item);
                             }
                         }
                     }
@@ -43,20 +43,36 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
+                        if (itemQualityIsGreaterThanZero(item)) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
+                                decrementItemQuality(item);
                             }
                         }
                     } else {
                         item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    if (itemQualityIsLessThanFifty(item)) {
+                        incrementItemQuality(item);
                     }
                 }
             }
         }
     }
+
+	private boolean itemQualityIsLessThanFifty(Item item) {
+		return item.quality < 50;
+	}
+
+	private void incrementItemQuality(Item item) {
+		item.quality = item.quality + 1;
+	}
+
+	private int decrementItemQuality(Item item) {
+		return item.quality = item.quality - 1;
+	}
+
+	private boolean itemQualityIsGreaterThanZero(Item item) {
+		return item.quality > 0;
+	}
 }
