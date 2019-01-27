1ba414a5200152158fbce4bb96413791a73eae9b
Eric Durbin <eadurbin@gmail.com>
2019-01-27 17:21:40 -0600

    isolate sulfuras

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -9,12 +9,14 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item: items) {
+        	if(itemIsSulfuras(item)) {
+        		continue;
+        	}
+        	
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (itemQualityIsGreaterThanZero(item)) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decrementItemQuality(item);
-                    }
+                	decrementItemQuality(item);
                 }
             } else {
                 if (itemQualityIsLessThanFifty(item)) {
@@ -36,17 +38,13 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (itemQualityIsGreaterThanZero(item)) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decrementItemQuality(item);
-                            }
+                        	decrementItemQuality(item);
                         }
                     } else {
                         item.quality = item.quality - item.quality;
@@ -59,6 +57,10 @@ class GildedRose {
             }
         }
     }
+
+	private boolean itemIsSulfuras(Item item) {
+		return item.name.equals("Sulfuras, Hand of Ragnaros");
+	}
 
 	private boolean itemQualityIsLessThanFifty(Item item) {
 		return item.quality < 50;
